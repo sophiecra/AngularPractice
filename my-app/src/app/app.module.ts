481030b8c1f98f10d462264d11f5891937f8ff9a
sophiecra <77420992+sophiecra@users.noreply.github.com>
2023-06-21 14:28:56 -0400

    feat: added mat toolbar

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    TopBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    ReactiveFormsModule, 
+    RouterModule.forRoot([
+      { path: '', component: 
+      ProductListComponent},
+    ]), AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
